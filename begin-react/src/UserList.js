3fa3f34f9e794a308ec1d00db4a24e7ba50e2871
hyun gyu <gusrb3164@gmail.com>
2020-11-29 17:32:36 +0900

    2020-11-29

---
+++ begin-react/src/UserList.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    return () => {
+      console.log(user);
+    };
+  });
+  return (
+    <div>
+      <b
+        style={{ cursor: "pointer", color: user.active ? "green" : "black" }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+}
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map((user) => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+}
+export default UserList;
